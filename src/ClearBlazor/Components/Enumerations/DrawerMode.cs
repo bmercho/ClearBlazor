f6d86ccec0f7567142a7bbb97633004d5ba4311b
Barry Merchant <bmercho99@gmail.com>
2024-09-22 12:21:55 +0930

    Fix for drawer

--- src/ClearBlazor/Components/Enumerations/DrawerMode.cs
+++ src/ClearBlazor/Components/Enumerations/DrawerMode.cs
@@ -10,12 +10,13 @@
         /// </summary>
         Permanent,
         /// <summary>
-        /// When the drawer is open it automatically closes when the overlay is clicked, provided the overlay is showing.
+        /// When the drawer is open it automatically closes when the overlay is clicked, 
+        /// provided the overlay is showing.
         /// </summary>
         Temporary,
         /// <summary>
-        /// The drawer closes if the Browser size is reduced to a DeviceSize of less than medium and reopens when the DeviceSize is 
-        /// greater than or equal to medium.
+        /// The drawer closes if the Browser size is reduced to a DeviceSize of less than 
+        /// medium and reopens when the DeviceSize is greater than or equal to medium.
         /// </summary>
         Responsive
     }
