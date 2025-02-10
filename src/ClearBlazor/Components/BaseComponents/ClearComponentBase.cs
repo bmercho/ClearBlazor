19530633ca6ddc16d5fdb00e8127424210298d8a
Barry Merchant <bmercho99@gmail.com>
2025-02-11 09:50:37 +1030

    DemoFixes

--- src/ClearBlazor/Components/BaseComponents/ClearComponentBase.cs
+++ src/ClearBlazor/Components/BaseComponents/ClearComponentBase.cs
@@ -469,10 +469,11 @@ namespace ClearBlazor
 
             if (!InScrollerX())
                 css += $"overflow-x:hidden; ";
-            //if (!InScrollerY() || this is Avatar || this is Image || this is MaterialIcon)
+//            if (!InScrollerY() || this is Avatar || this is Image || this is MaterialIcon)
+            if (this is Avatar || this is Image || this is MaterialIcon || this is ToolTip)
                 // The line below affected images in a scroll viewer
                 // but is required for text in a scroll viewer
-            //    css += $"overflow-y:hidden; ";
+                css += $"overflow-y:hidden; ";
             css += $"white-space: nowrap; ";
 
             return css;
