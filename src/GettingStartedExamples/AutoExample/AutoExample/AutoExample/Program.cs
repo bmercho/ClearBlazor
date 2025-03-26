5bcc62009a9e1c48b57bca88c71607072768f353
Barry Merchant <bmercho99@gmail.com>
2025-03-26 13:14:53 +1030

    Added getting started examples

---
+++ src/GettingStartedExamples/AutoExample/AutoExample/AutoExample/Program.cs
@@ -0,0 +1,31 @@
+﻿using AutoExample.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(AutoExample.Client._Imports).Assembly);
+
+app.Run();
