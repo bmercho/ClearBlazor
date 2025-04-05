8ca4a21448b8f45c863c3b6183a96154508ad5b2
Barry Merchant <bmercho99@gmail.com>
2025-04-05 11:17:55 +1030

    Doco

--- src/ClearBlazorTestCore/Pages/Components/Forms/AutoForm/Doco/AutoFormDocsInfo.cs
+++ src/ClearBlazorTestCore/Pages/Components/Forms/AutoForm/Doco/AutoFormDocsInfo.cs
@@ -17,6 +17,7 @@ namespace ClearBlazorTest
         {
             new ApiComponentInfo("HasLabelColumn", "bool", "false", "Indicates whether a label column is present. Defaults to false.\r"),
             new ApiComponentInfo("LabelValueCols", "string", "*,*", "Defines the columns for label values in a string format, defaulting to '*,*'. This allows for flexible\rcolumn configuration.\r"),
+            new ApiComponentInfo("RowSpacing", "int", "20", "Spacing between rows\r"),
             new ApiComponentInfo("InputContainerStyle", "<a href=InputContainerStyleApi>InputContainerStyle</a>", "InputContainerStyle.Underlined", "Defines the style of the input container, defaulting to 'Underlined'. It allows customization of the input's\rappearance.\r"),
             new ApiComponentInfo("Size", "<a href=SizeApi>Size</a>", "Size.Normal", "Defines the size of a component, with a default value of 'Normal'. \r"),
             new ApiComponentInfo("Immediate", "bool", "false", "Indicates whether the input is immediate. ie Changes the Value as soon as input is received,\rotherwise, Value is updated when the user presses Enter or the input loses focus.\rDefaults to false.\r"),
---
+++ src/ClearBlazorTestCore/ResourceFile.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+
+namespace ClearBlazor
+{
+    public static class ResourceFile
+    {
+        public static string ReadResourceFile(string fileName)
+        {
+            string result = string.Empty;
+            Stream? stream = null;
+            try
+            {
+                var assembly = Assembly.GetExecutingAssembly();
+                var resourceName = assembly.GetManifestResourceNames().Single(str => str.EndsWith(fileName));
+
+                stream = assembly.GetManifestResourceStream(resourceName);
+                if (stream != null)
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        result = reader.ReadToEnd();
+                    }
+            }
+            catch (Exception)
+            {
+                result = string.Empty;
+            }
+            finally
+            {
+                stream?.Close();
+            }
+            return result;
+        }
+    }
+}
